ab7c66b6990b4617ab4b9feea527df298aa9ee1a
Myself-Ashish-Sarker <myselfashishsarker@gmail.com>
2024-07-02 22:33:17 +0600

    navbar addded but need further customization

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../Components/Root";
+import Home from "../Components/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <Root></Root>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            }
+        ]
     },
 ]);
 
